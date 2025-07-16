05c0ad6fa2d7c3b61a7147a56c841ed3aa5ea8aa
chandhini07 <chandini7104@gmail.com>
2025-07-16 18:25:32 +0530

    Update py code.py

--- py code.py
+++ py code.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("titanic.csv")
 
@@ -12,16 +13,17 @@ print(df.isnull().sum())
 print("\n📈 Basic Statistics:")
 print(df.describe())
 
-
 print("\n👥 Average Age by Survival Status:")
 print(df.groupby('Survived')['Age'].mean())
-
 
 print("\n🛳️ Top 5 Embarkation Ports:")
 print(df['Embarked'].value_counts().head(5))
 
-
-
 summary = df.describe()
 summary.to_excel("titanic_summary.xlsx")
 print("\n📁 Summary exported to titanic_summary.xlsx")
+
+df['Survived'].value_counts().plot(kind='bar', title='Survival Count')
+plt.xlabel('Survived (0 = No, 1 = Yes)')
+plt.ylabel('Count')
+plt.show()
